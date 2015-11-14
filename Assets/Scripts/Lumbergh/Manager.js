1c832859cbe45e0479d848c4747726425a252160
topher wilson <tophermade@gmail.com>
2015-11-14 02:39:55 -0700

    procam 2d added and looking swell

--- Assets/Scripts/Lumbergh/Manager.js
+++ Assets/Scripts/Lumbergh/Manager.js
@@ -1,10 +1,15 @@
 ﻿#pragma strict
 
 var puckObject 			: GameObject;
+var spawnParent 		: GameObject;
 var spawns 				: GameObject[];
+
 var lastSpawnPosition 	: GameObject;
 var obstacle 			: GameObject;
 var puck 				: GameObject;
+
+var highScoreSpot 		: GameObject;
+var highScorePrefab 	: GameObject;
 
 var scoreDisplay 		: GameObject;
 
@@ -12,6 +17,8 @@ var playing 			: boolean 		= false;
 
 var score 				: int 			= 0;
 var lastSpawnTime  		: float 		= 0;
+
+var startingPosition 	: Vector2 		= Vector2(0,0);
 
 
 function UpdateScoreDisplay(){
@@ -31,21 +38,58 @@ function SpawnObstacle(){
 }
 
 
+
+// round helpers
+function DestroyObstacles(){
+
+}
+
+function ProcessScore(){
+	if(PlayerPrefs.GetInt("Highscore") > score){
+		
+	} else {
+		PlayerPrefs.SetInt("Highscore", score);
+	}
+}
+
+
+// rounds
 function EndRound(){
-	
+	playing = false;
+	ProcessScore();
 }
 
 
 function SetupRound(){
 	score = 0;
 	playing = true;
+	DestroyObstacles();
+	puck.transform.position = startingPosition;
 }
 
 
+function StartRound(){
+	SetupRound();
+	playing = true;
+	puck.SendMessage("StartRound");
+
+	if(PlayerPrefs.GetInt("Highscore")){
+		if(PlayerPrefs.GetInt("Highscore") > 0){
+			highScoreSpot = Instantiate(highScorePrefab, Vector3(0, PlayerPrefs.GetInt("Highscore") *5, 0), Quaternion.identity);
+		}
+	} else {	
+		PlayerPrefs.SetInt("Highscore",0);
+	}
+}
+
+
+
+// unity standard
 function Start () {
 	if(!puck){
 		puck = GameObject.Find("Puck");
 	}
+	StartRound();
 	SpawnObstacle();
 }
 
@@ -60,7 +104,7 @@ function Update () {
 		}
 	}
 
-	if(lastSpawnTime < Time.time){
+	if(playing && lastSpawnTime < Time.time){
 		lastSpawnTime = Time.time + .6;
 		SpawnObstacle();
 	}
--- Assets/Scripts/Puck/Puck.js
+++ Assets/Scripts/Puck/Puck.js
@@ -4,6 +4,7 @@ var lumbergh 		: GameObject;
 var ripple 			: GameObject;
 var rippleSpawn 	: GameObject;
 
+var playing 		: boolean 		= false;
 var goingRight 		: boolean 		= true;
 
 var currentSpeed 	: Vector2;
@@ -18,15 +19,17 @@ function DestroyRipple(ripple: GameObject){
 
 
 function ChangeDirection(){
-	if(goingRight){
-		goingRight = false;
-		currentSpeed = Vector2(-speed,speed);
-	} else{
-		goingRight =  true;
-		currentSpeed = Vector2(speed,speed);
+	if(playing){
+		if(goingRight){
+			goingRight = false;
+			currentSpeed = Vector2(-speed,speed);
+		} else{
+			goingRight =  true;
+			currentSpeed = Vector2(speed,speed);
+		}
+		var newRipple = Instantiate(ripple, rippleSpawn.transform.position, Quaternion.identity);
+		DestroyRipple(newRipple);
 	}
-	var newRipple = Instantiate(ripple, rippleSpawn.transform.position, Quaternion.identity);
-	DestroyRipple(newRipple);
 }
 
 
@@ -39,19 +42,32 @@ function OnCollisionEnter2D(other : Collision2D){
 
 	} else if(tag == "Obstacle"){
 		print("it was an obstacle");
+		playing = false;
+		Camera.main.SendMessage("ShakeUsingPreset", "SmallShake");
 		lumbergh.SendMessage("EndRound");
 		currentSpeed = Vector2(0,0);
 	}
 }
 
 
-function Start () {
+
+function StartRound(){
 	currentSpeed = Vector2(speed,speed);
+	playing = true;
+}
+
+
+
+function Start () {
 	lumbergh = GameObject.Find("Lumbergh");
 }
 
 
 
 function Update () {
-	GetComponent(Rigidbody2D).velocity = currentSpeed;
+	if(playing){
+		GetComponent(Rigidbody2D).velocity = currentSpeed;
+	} else {
+		GetComponent(Rigidbody2D).velocity = Vector2(0,0);
+	}
 }
